8ab850c552ba38302a423d88a3b260d022d0a470
AdamDX1337 <cough5985@gmail.com>
2023-10-10 13:19:52 +1100

    Add project files.

---
+++ RevisionQuestions/Node.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeTest
+{
+    public class Node
+    {
+        public int num;
+        public Node next;
+        public Node(int number)
+        {
+            num = number;
+            next = null;
+        }
+    }
+
+    public class LinkedListA
+    {
+        public static Node mainNode;
+
+        public static void Push(int number)
+        {
+            Node node = new Node(number);
+            if (mainNode != null)
+            {
+                Node currentNode = mainNode;
+                while (currentNode.next != null)
+                {
+                    currentNode = currentNode.next;
+                }
+                currentNode.next = node;
+            }
+            else
+            {
+                mainNode = node;
+            }
+        }
+
+        public static void Pop()
+        {
+            if (mainNode != null)
+            {
+                Node currentNode = mainNode;
+                while (currentNode.next.next != null)
+                {
+                    currentNode = currentNode.next;
+                }
+                currentNode.next = null;
+            }
+            else
+            {
+                Console.WriteLine("Empty List");
+            }
+        }
+
+        public static void Peek()
+        {
+            if (mainNode == null)
+            {
+                Console.WriteLine("Empty List");
+            }
+            else
+            {
+                Node current = mainNode;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                Console.WriteLine(current.num);
+            }
+        }
+    }
+}
---
+++ RevisionQuestions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace NodeTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            LinkedListA.Push(2);
+            LinkedListA.Push(2);
+            LinkedListA.Push(3);
+            LinkedListA.Pop();
+            LinkedListA.Peek();
+            
+        }
+    }
+}
